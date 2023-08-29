f56d29c1ad2626e4332ec9c32616301191441d51
mhkarazeybek <mhkarazeybek@gmail.com>
2023-08-30 00:32:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Automated Setup Script for CUDA, cuDNN, and OpenCV Installation
+
+This script automates the installation process for CUDA, cuDNN, and OpenCV on an Ubuntu system. It provides a streamlined way to ensure that the required dependencies and libraries are properly installed for deep learning and computer vision development.
+
+## Requirements
+
+- Ubuntu operating system (tested on Ubuntu 20.04)
+- NVIDIA GPU with CUDA support
+
+## Usage
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/mhkarazeybek/cuda-cudnn-opencv-installation.git
+2. Navigate to the project directory:
+   ```bash
+   cd cuda-cudnn-opencv-installation
+3. Make the script executable:
+    ```bash
+    chmod +x install_script.sh
+4. Run the script with necessary arguments to specify the versions:
+
+    Replace <CMAKE_VERSION>, <CUDA_VERSION>, <CUDNN_VERSION>, <OPENCV_VERSION>, and <UBUNTU_VERSION> with the desired versions of CMake, CUDA, cuDNN, OpenCV, and Ubuntu respectively.
+    ```bash
+    ./install_script.sh --cmake <CMAKE_VERSION> --cuda <CUDA_VERSION> --cudnn <CUDNN_VERSION> --opencv <OPENCV_VERSION> --ubuntu <UBUNTU_VERSION>
+5. Follow the on-screen instructions and prompts.
+
+## Notes
+- The script first uninstalls existing CUDA, cuDNN, and NVIDIA drivers if they are detected.
+- It then installs the necessary NVIDIA drivers automatically using the ubuntu-drivers command.
+- The script compares the installed CMake version with the provided version. If the installed version is older, it removes the old version and installs the specified CMake version.
+- CUDA, cuDNN, and OpenCV are installed based on the provided version numbers.
+- After successful installation, the script verifies the OpenCV installation.
---
+++ install_cuda_cudnn_opencv.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Usage function
+usage() {
+    echo "Usage: $0 --cmake <CMAKE_VERSION> --cuda <CUDA_VERSION> --cudnn <CUDNN_VERSION> --opencv <OPENCV_VERSION> --ubuntu <UBUNTU_VERSION>"
+    exit 1
+}
+
+# Display usage if no arguments are provided
+if [ $# -eq 0 ]; then
+    usage
+fi
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --cmake)
+            CMAKE_VERSION=$2
+            shift 2
+            ;;
+        --cuda)
+            CUDA_VERSION=$2
+            shift 2
+            ;;
+        --cudnn)
+            CUDNN_VERSION=$2
+            shift 2
+            ;;
+        --opencv)
+            OPENCV_VERSION=$2
+            shift 2
+            ;;
+        --ubuntu)
+            UBUNTU_VERSION=$2
+            shift 2
+            ;;
+        --help)
+            usage
+            ;;
+        *)
+            echo "Invalid option: $1"
+            usage
+            ;;
+    esac
+done
+
+# Check for required parameters
+if [ -z "${CMAKE_VERSION}" ] || [ -z "${CUDA_VERSION}" ] || [ -z "${CUDNN_VERSION}" ] || [ -z "${OPENCV_VERSION}" ] || [ -z "${UBUNTU_VERSION}" ]; then
+    usage
+fi
+
+# Uninstall existing CUDA and NVIDIA drivers
+sudo apt-get --purge remove "*cublas*" "*cufft*" "*curand*" \
+"*cusolver*" "*cusparse*" "*npp*" "*nvjpeg*" "cuda*" "nsight*" "*nvidia*"
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /usr/local/cuda*
+
+# Install NVIDIA drivers
+sudo ubuntu-drivers autoinstall
+
+# Check installed cmake version
+INSTALLED_CMAKE_VERSION=$(cmake --version | grep -oE "[0-9]+\.[0-9]+\.[0-9]+" | head -n 1)
+echo "Installed CMake version: ${INSTALLED_CMAKE_VERSION}"
+
+# Compare installed cmake version with required cmake version
+if [[ $(echo "${INSTALLED_CMAKE_VERSION} < ${CMAKE_VERSION}" | bc -l) -eq 1 ]]; then
+    echo "Removing old CMake version: ${INSTALLED_CMAKE_VERSION}"
+    sudo apt-get remove cmake
+    echo "Installing CMake version: ${CMAKE_VERSION}"
+    sudo apt-get install -y cmake=${CMAKE_VERSION}-*
+else
+    echo "CMake version is up to date: ${INSTALLED_CMAKE_VERSION}"
+fi
+
+# Install CUDA
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/x86_64/cuda-ubuntu${UBUNTU_VERSION}.pin
+sudo mv cuda-ubuntu${UBUNTU_VERSION}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/local_installers/cuda-repo-ubuntu${UBUNTU_VERSION}-${CUDA_VERSION}-local_${CUDA_VERSION}-470.57.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu${UBUNTU_VERSION}-${CUDA_VERSION}-local_${CUDA_VERSION}-470.57.02-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu${UBUNTU_VERSION}-${CUDA_VERSION}-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+# Install cuDNN
+wget https://developer.download.nvidia.com/compute/redist/cudnn/v${CUDNN_VERSION}/cudnn-${CUDNN_VERSION}-linux-x64-v${CUDNN_VERSION}.tgz
+tar -xzvf cudnn-${CUDNN_VERSION}-linux-x64-v${CUDNN_VERSION}.tgz
+sudo cp cuda/include/cudnn*.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
+sudo cp cuda/include/cudnn.h /usr/include
+sudo cp cuda/lib64/libcudnn* /usr/lib/x86_64-linux-gnu/
+sudo chmod a+r /usr/lib/x86_64-linux-gnu/libcudnn*
+
+# Download and compile OpenCV
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y cmake pkg-config unzip yasm git checkinstall \
+libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev \
+libswscale-dev libavresample-dev libgstreamer1.0-dev \
+libgstreamer-plugins-base1.0-dev libxvidcore-dev x264 libx264-dev \
+libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev \
+libopencore-amrnb-dev libopencore-amrwb-dev \
+libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils \
+libgtk-3-dev libtbb-dev libatlas-base-dev gfortran
+
+cd ~/Downloads
+wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/${OPENCV_VERSION}.zip
+unzip opencv.zip
+cd opencv-${OPENCV_VERSION}
+mkdir build
+cd build
+
+# Configure OpenCV build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_INSTALL_PREFIX=/usr/local/bin \
+-D WITH_TBB=ON \
+-D ENABLE_FAST_MATH=1 \
+-D CUDA_FAST_MATH=1 \
+-D WITH_CUBLAS=1 \
+-D WITH_CUDA=ON \
+-D BUILD_opencv_cudacodec=OFF \
+-D WITH_CUDNN=ON \
+-D OPENCV_DNN_CUDA=ON \
+-D WITH_V4L=ON \
+-D WITH_QT=OFF \
+-D WITH_OPENGL=ON \
+-D WITH_GSTREAMER=ON \
+-D OPENCV_GENERATE_PKGCONFIG=ON \
+-D OPENCV_PC_FILE_NAME=opencv.pc \
+-D OPENCV_ENABLE_NONFREE=ON \
+-D OPENCV_PYTHON3_INSTALL_PATH=/usr/local/lib/python3.6/dist-packages \
+-D PYTHON_EXECUTABLE=/usr/bin/python3 \
+-D INSTALL_PYTHON_EXAMPLES=OFF \
+-D INSTALL_C_EXAMPLES=OFF \
+-D BUILD_EXAMPLES=OFF ..
+
+# Compile and install OpenCV
+nproc=$(nproc)  # Get number of CPU cores
+make -j${nproc}
+sudo make install
+
+# Configure library paths for OpenCV
+sudo /bin/bash -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+
+# Verify OpenCV installation
+python3 -c "import cv2; print(cv2.getBuildInformation())"
